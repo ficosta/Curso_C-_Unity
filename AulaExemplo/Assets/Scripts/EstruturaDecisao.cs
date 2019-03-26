044231ccb6c6624b7ce7856c8850cfb15ad4db96
Felipe <felipeiasi@gmail.com>
2019-03-26 19:13:57 -0300

    Aula decisão IF / SWITCH

---
+++ AulaExemplo/Assets/Scripts/EstruturaDecisao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstruturaDecisao : MonoBehaviour
+{
+
+    public uint vidaJogador;
+    public bool podeAtacar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaJogador = 100;
+        podeAtacar = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (vidaJogador > 0)
+        {
+            podeAtacar = true;
+        }
+        else
+        {
+            podeAtacar = false;
+        }
+        print(podeAtacar);
+    }
+}
---
+++ AulaExemplo/Assets/Scripts/Switch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    private string Inimigo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inimigo = "Dragão";    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Inimigo)
+        {
+            case "Dragão":
+                print("Enfrente o Dragão");
+                break;
+            case "Tigre":
+                print("Enfrente o Tigre");
+                break;
+            case "Leão":
+                print("Enfrente o Leão");
+                break;
+            default:
+                print("Inimigo inesperado");
+                break;
+        }
+    }
+}
