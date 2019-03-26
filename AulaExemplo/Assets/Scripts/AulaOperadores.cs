5aecaca1da9090e1dd9d2cb5df148b0cff045353
Felipe <felipeiasi@gmail.com>
2019-03-26 18:19:16 -0300

    Aula 8 Operadores

---
+++ AulaExemplo/Assets/Scripts/AulaOperadores.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AulaOperadores : MonoBehaviour
+{
+
+    private     int     vidaMaximaJogador, vidaAtualJogador, dano, porcaoVida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaAtualJogador = 100 + 50;
+        print(vidaAtualJogador);
+
+        vidaMaximaJogador = 100;
+        vidaAtualJogador = vidaMaximaJogador;
+        print(vidaAtualJogador);
+        dano = 15;
+        vidaAtualJogador = vidaAtualJogador - dano;
+        porcaoVida = 10;
+        vidaAtualJogador += porcaoVida;
+        print(vidaAtualJogador);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
