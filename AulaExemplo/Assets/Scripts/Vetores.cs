644fdb98456bbc4ed37451d853c73de9f5949d4a
Felipe <felipeiasi@gmail.com>
2019-03-26 19:42:21 -0300

    Vetores

---
+++ AulaExemplo/Assets/Scripts/Vetores.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vetores : MonoBehaviour
+{
+
+    [SerializeField]
+    private string[] Inimigos1 = { "Dragão", "Tigre", "Leao", "Cobra" };
+    // Start is called before the first frame update
+    public uint indice = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (indice >= Inimigos1.Length)
+        {
+            print("Nao existem tantos inimigos assim");
+        }
+        else
+        {
+            print(Inimigos1[indice]);
+        }
+    }
+}
